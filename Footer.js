f0e0af43aad11ea26e87a751405c36ab0c30b264
kappukalpana20-del <kappukalpana20@gmail.com>
2025-09-18 15:56:43 +0530

    Add files via upload

---
+++ Footer.js
@@ -0,0 +1,7 @@
+function Footer(){
+    return(
+        <footer style={{background:"#222",color:"white",padding:"10px",marginTop:"20px"}}>
+            <p>&copy;2025My React App | All Rights Reserved</p>
+        </footer>
+    )
+}
---
+++ imageDisplay.js
@@ -0,0 +1,14 @@
+function imageDisplay (){
+    return(
+        <div style={{textAlign:"center",margin:"20px"}}>
+            <h2>welcome to my app</h2>
+            <img
+                src="c:\users\Devika\html files\images/11.jpg"
+                alt="Sample"
+                style={{borderRadius:"10px",boxShadow:"0px 4px 8px rgba(0,0,0,0.2)"}}
+            />
+
+        </div>
+    )
+}
+export default imageDisplay;
